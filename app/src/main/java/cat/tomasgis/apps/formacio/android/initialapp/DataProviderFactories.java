2441c892b38f9e95c5dba97229cd21b209e717aa
TomasGiS <me@tomasgis.cat>
2016-07-21 13:51:38 +0200

    Data Provider Factory to switch data source

--- app/src/main/java/cat/tomasgis/apps/formacio/android/initialapp/DataProviderFactories.java
+++ app/src/main/java/cat/tomasgis/apps/formacio/android/initialapp/DataProviderFactories.java
@@ -1,7 +1,22 @@
 package cat.tomasgis.apps.formacio.android.initialapp;
 
+import android.content.Context;
+
+import cat.tomasgis.apps.formacio.android.initialapp.database.TouristPlaceDBProvider;
+import cat.tomasgis.apps.formacio.android.initialapp.interfaces.ITouristDataAccess;
+import cat.tomasgis.apps.formacio.android.initialapp.provider.DataProvider;
+
 /**
  * Created by TomasGiS on 21/7/16.
  */
 public class DataProviderFactories {
+
+    public enum TouristicDataSourceType {StaticData, DabaseData};
+
+    public ITouristDataAccess getDataSource(Context context, TouristicDataSourceType type)
+    {
+        if (type == TouristicDataSourceType.StaticData) return DataProvider.getInstance();
+        else if (type == TouristicDataSourceType.DabaseData) return TouristPlaceDBProvider.getInstance(context);
+        else return null;
+    }
 }
